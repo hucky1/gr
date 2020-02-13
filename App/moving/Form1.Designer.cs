9e7af9d5a01082d14e74a99843c294cce7697080
hucky1 <48276792+hucky1@users.noreply.github.com>
2020-02-13 13:58:12 +0300

    Add files via upload

---
+++ App/moving/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace moving
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trbFigSize = new System.Windows.Forms.TrackBar();
+            this.btnDefaultA = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblXcYc = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trbFigRotRate = new System.Windows.Forms.TrackBar();
+            this.trbFigSpeed = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigRotRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trbFigSize
+            // 
+            this.trbFigSize.Location = new System.Drawing.Point(633, 37);
+            this.trbFigSize.Maximum = 140;
+            this.trbFigSize.Minimum = 80;
+            this.trbFigSize.Name = "trbFigSize";
+            this.trbFigSize.Size = new System.Drawing.Size(150, 45);
+            this.trbFigSize.TabIndex = 22;
+            this.trbFigSize.Value = 80;
+            this.trbFigSize.Scroll += new System.EventHandler(this.trbFigSize_Scroll);
+            // 
+            // btnDefaultA
+            // 
+            this.btnDefaultA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDefaultA.Location = new System.Drawing.Point(642, 387);
+            this.btnDefaultA.Name = "btnDefaultA";
+            this.btnDefaultA.Size = new System.Drawing.Size(137, 41);
+            this.btnDefaultA.TabIndex = 21;
+            this.btnDefaultA.Text = "Размер по умолчанию";
+            this.btnDefaultA.UseVisualStyleBackColor = true;
+            this.btnDefaultA.Click += new System.EventHandler(this.btnDefaultA_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDraw.Location = new System.Drawing.Point(642, 332);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(137, 49);
+            this.btnDraw.TabIndex = 20;
+            this.btnDraw.Text = "Нарисовать фигуру";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(642, 290);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 36);
+            this.btnStart.TabIndex = 19;
+            this.btnStart.Text = "Движение фигуры";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(642, 243);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(137, 40);
+            this.btnStop.TabIndex = 18;
+            this.btnStop.Text = "Остановить вращение";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 412);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblXcYc
+            // 
+            this.lblXcYc.AutoSize = true;
+            this.lblXcYc.Location = new System.Drawing.Point(639, 212);
+            this.lblXcYc.Name = "lblXcYc";
+            this.lblXcYc.Size = new System.Drawing.Size(155, 13);
+            this.lblXcYc.TabIndex = 29;
+            this.lblXcYc.Text = "Координаты центра тяжести:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(639, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 15);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Скорость вращения";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(639, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Размер фигуры";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(642, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 25;
+            // 
+            // trbFigRotRate
+            // 
+            this.trbFigRotRate.Location = new System.Drawing.Point(634, 152);
+            this.trbFigRotRate.Minimum = 1;
+            this.trbFigRotRate.Name = "trbFigRotRate";
+            this.trbFigRotRate.Size = new System.Drawing.Size(149, 45);
+            this.trbFigRotRate.TabIndex = 24;
+            this.trbFigRotRate.Value = 10;
+            this.trbFigRotRate.Scroll += new System.EventHandler(this.trbFigRotRate_Scroll);
+            // 
+            // trbFigSpeed
+            // 
+            this.trbFigSpeed.Location = new System.Drawing.Point(634, 90);
+            this.trbFigSpeed.Minimum = 1;
+            this.trbFigSpeed.Name = "trbFigSpeed";
+            this.trbFigSpeed.Size = new System.Drawing.Size(149, 45);
+            this.trbFigSpeed.TabIndex = 23;
+            this.trbFigSpeed.Value = 10;
+            this.trbFigSpeed.Scroll += new System.EventHandler(this.trbFigSpeed_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(639, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 15);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Скорость фигуры";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trbFigSize);
+            this.Controls.Add(this.btnDefaultA);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblXcYc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trbFigRotRate);
+            this.Controls.Add(this.trbFigSpeed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigRotRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbFigSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trbFigSize;
+        private System.Windows.Forms.Button btnDefaultA;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblXcYc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trbFigRotRate;
+        private System.Windows.Forms.TrackBar trbFigSpeed;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ App/moving/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moving
+{
+    public partial class Form1 : Form
+    {
+        const int default_a = 120;
+        double i =1;
+        int dir = 1; //направление движения по молчанию вперед
+        double dx = 0.1; //приращение по х при движении
+        double xc, yc; //координаты центра
+        int a; //размер фигуры и координаты центра
+        int v = 1; //скорость движения
+        Point[] p1 = new Point[4]; //массив точек для ромба
+        Point[] p2 = new Point[4]; //массив точек для квадрата
+        double[] x = new double[8];
+        double[] y = new double[8];
+        double fi; // угол, на который фигура поворачивается при обновлении таймера
+        double c_fi = 0; // угол, на который фигура повернулась
+        int rotation_rate = 1; // скорость вращения
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = default_a; // задаем размер фигуры
+                           // Вычисляем координаты центра, чтобы фигура находилась в левом нижнем углу
+                           // pictureBox1.W6idth и pictureBox1.Height - ширина и высота pictureBox
+            xc = Convert.ToInt32((a / 2.0) * Math.Cos(Math.PI / 6));
+            yc = Convert.ToInt32(pictureBox1.Height - a / 2.0);
+            fi = 3 * dir * Math.PI / 180;
+            // начальное положение ползунков
+            trbFigRotRate.Value = rotation_rate;
+            trbFigSize.Value = a;
+            trbFigSpeed.Value = v;
+        }
+        private double[] x_rot(double[] x0, double[] y0)
+        {
+            double[] x1 = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x1[i] = (x0[i]) * Math.Cos(c_fi) - (y0[i]) * Math.Sin(c_fi);
+            }
+            return x1;
+        }
+
+        // Расчет координат y точек при вращении
+        private double[] y_rot(double[] x0, double[] y0)
+        {
+            double[] y1 = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y1[i] = (x0[i]) * Math.Sin(c_fi) + (y0[i]) * Math.Cos(c_fi);
+            }
+            return y1;
+        }
+        private byte colorInc()
+        {
+            try
+            {
+                double col = 255 * (xc - (a / 2) * Math.Cos(Math.PI / 6)) /
+                    (pictureBox1.Width - a * Math.Cos(Math.PI / 6));
+                return Convert.ToByte(col);
+            }
+            catch { return 0; }
+        }
+
+        // Расчет убывающих цветовых координат
+        private byte colorDec()
+        {
+            try
+            {
+                double col = -255 * ((xc - (a / 2) * Math.Cos(Math.PI / 6)) /
+                                  (pictureBox1.Width - a * Math.Cos(Math.PI / 6))) + 255;
+                return Convert.ToByte(col);
+            }
+            catch { return 0; }
+        }
+        private void btnDraw_Click(object sender, EventArgs e) => Draw();
+
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (xc+a > pictureBox1.Width)       
+                dir = -1;//меняем направление 
+            if (xc < 0)
+                dir = 1;
+
+            c_fi += fi * rotation_rate;
+
+            Draw(); //перерисовываем фигуру   
+
+            dx = v * dir; //приращение координаты зависит от направления и скорости       
+            fi = 1 * dir * Math.PI / (180); //угол меняет направление         
+            xc = xc + dx;
+            // yc = ((pictureBox1.Height -2*a)/2* Math.Sin(i)-200); //движение по синусоиде   
+            yc = pictureBox1.Height  + ((pictureBox1.Height - 4 * a)) * Math.Sin(0.04 * (xc - a / 2));
+            i +=0.05;
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) => timer1.Start();
+      
+
+        private void trbFigRotRate_Scroll(object sender, EventArgs e)
+        {
+            rotation_rate = trbFigRotRate.Value;
+        }
+
+        private void trbFigSpeed_Scroll(object sender, EventArgs e)
+        {
+            v = trbFigSpeed.Value;
+        }
+
+        private void btnDefaultA_Click(object sender, EventArgs e)
+        {
+            a = default_a;
+            if (!timer1.Enabled) //проверка включенности таймера(true-таймер выключен)
+            {
+                xc = Convert.ToInt32((a / 2.0) * Math.Cos(Math.PI / 6));
+                yc = Convert.ToInt32(pictureBox1.Height - a / 2.0);
+                Draw();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            xc = Convert.ToInt32((a / 2.0) * Math.Cos(Math.PI / 6));
+            yc = Convert.ToInt32(pictureBox1.Height - a / 2.0);
+            c_fi = 0;
+            Draw();
+        }
+
+        private void trbFigSize_Scroll(object sender, EventArgs e)
+        {
+            a = trbFigSize.Value;
+        }
+
+        private void Draw()
+        {
+            Graphics g = pictureBox1.CreateGraphics(); // Создание графического объекта
+            Brush br1 = new SolidBrush(Color.FromArgb(colorDec(), colorDec(), colorInc())); // кисть ромба
+            Brush br2 = new SolidBrush(Color.FromArgb(colorInc(), colorDec(), colorInc())); //кисть квадрата
+            g.Clear(SystemColors.Control); // стирание
+
+            //рассчет координат точек фигуры относительно центра
+            //квадрат
+            x[0] = Convert.ToInt32((xc+a / 4));
+            y[0] = Convert.ToInt32(yc+pictureBox1.Height - ((a / 2) + (a / 4)))-500;
+            x[1] = Convert.ToInt32((xc+a / 2) + (a / 4));
+            y[1] = Convert.ToInt32(yc+pictureBox1.Height - ((a / 2) + (a / 4)))-500;
+            x[2] = Convert.ToInt32((xc+a / 2) + (a / 4));
+            y[2] = Convert.ToInt32(yc+pictureBox1.Height - (a / 4))-500;
+            x[3] = Convert.ToInt32(xc+a / 4);
+            y[3] = Convert.ToInt32(yc+pictureBox1.Height - (a / 4))-500;
+            //ромб
+            x[4] = Convert.ToInt32(xc+a / 2);
+            y[4] = Convert.ToInt32(yc+pictureBox1.Height - a)-500;
+            x[5] = Convert.ToInt32(xc+a);
+            y[5] = Convert.ToInt32(yc+pictureBox1.Height - (a / 2))-500;
+            x[6] = Convert.ToInt32(xc+a / 2);
+            y[6] = Convert.ToInt32(yc+pictureBox1.Height)-500;
+            x[7] = Convert.ToInt32(xc+0);
+            y[7] = Convert.ToInt32(yc+pictureBox1.Height - (a / 2))-500;
+
+            
+            double[] temp = new double[8];
+            temp = x; //копируем массив x во временную переменную         
+           x = x_rot(temp, y);
+            y = y_rot(temp, y);
+
+            //заполняем массив точек квадрата
+            p1[0] = new Point((int)(x[0]), (int)(y[0]));
+            p1[1] = new Point((int)(x[1]), (int)(y[1]));
+            p1[2] = new Point((int)(x[2]), (int)(y[2]));
+            p1[3] = new Point((int)(x[3]), (int)(y[3]));
+
+            //заполняем массив точек ромба
+            p2[0] = new Point((int)(x[4]), (int)(y[4]));
+            p2[1] = new Point((int)(x[5]), (int)(y[5]));
+            p2[2] = new Point((int)(x[6]), (int)(y[6]));
+            p2[3] = new Point((int)(x[7]), (int)(y[7]));
+
+
+            g.FillPolygon(br2, p2);
+            g.FillPolygon(br1, p1);
+            lblXcYc.Text = String.Format("X: {0:0.00}, Y: {1:0.00}", xc, yc); // вывод координат центра тяжести точки
+        }
+    }
+}
